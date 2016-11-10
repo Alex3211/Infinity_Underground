91ba86b28febe262148aa0c0f48fbef02772fec0
VexTech <dylanherve93@gmail.com>
2016-11-10 21:06:20 +0100

    Added Solution

---
+++ Kepler-22_B.Tests/KeplerTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Kepler_22_B.Tests
+{
+    [TestFixture]
+    public class KeplerTests
+    {
+        [Test]
+        public void test()
+        {
+            Assert.That(true);
+        }
+    }
+}
